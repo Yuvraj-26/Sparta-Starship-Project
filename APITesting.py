f64d12d038457ba016c5f519e8fd14dc714dbeef
Andrew Carver <andyc2901@gmail.com>
2023-09-14 16:20:10 +0100

    Updated the tests as a class and added further tests

--- APITesting.py
+++ APITesting.py
@@ -1,34 +1,41 @@
 import unittest
 import pymongo
-
-def test_starship_api_request(starship_api):
-    """
-    Expecting a json input of the results from the starships api pull.
-    Should be length 36 which is what is checked here.
-    Also checks the length of the first index in the json.
-    """
-    assert len(starship_api) == 36
-    assert len(starship_api[0]) == 18
+import json
 
 
-def test_url_from_api(pilot_url):
-    """
-    Expects the input from the 5th star ship in the API(index 4, Millenium Falcon)
-    """
-    assert pilot_url[5][1] == ["https://swapi.dev/api/people/13/",
-                               "https://swapi.dev/api/people/14/",
-                               "https://swapi.dev/api/people/25/",
-                               "https://swapi.dev/api/people/31/"]
+class Tests(unittest.TestCase):
+    def test_starship_api_request(self):
+        """
+        Takes the "fetch_starships" function and checks the length of both the json and the objects in the json
+        """
+        actual_len = len(fetch_starships())
+        actual_len_firstdata = len(fetch_starships()[0])
+        expected1 = 36
+        expected2 = 18
+        self.assertEqual(actual_len, expected1)
+        self.assertEqual(actual_len_firstdata, expected2)
 
+    def test_url_from_api(self):
+        """
+        Takes the "function" function and checks the pilots are displayed as URLs
+        """
+        actual = function()['Millennium Falcon']
+        expected = ["https://swapi.dev/api/people/13/",
+                    "https://swapi.dev/api/people/14/",
+                    "https://swapi.dev/api/people/25/",
+                    "https://swapi.dev/api/people/31/"]
+        self.assertEqual(actual, expected)
 
-def test_pilot_api_request(pilot_api):
-    """
-    Expects a json input from the pilot's api request.
-    Checks the length of the character json and checks it has the name category
-    Should be length 16
-    """
-    assert len(pilot_api) == 16
-    assert pilot_api['name'] == 'Luke Skywalker'
+    def test_pilot_api_request(self):
+        """
+        Takes the "function" function and checks the URLs have been replaces by character names
+        """
+        actual = function()['Millennium Falcon']
+        expected = ["Chewbacca",
+                    "Han Solo",
+                    "Lando Calrissian",
+                    "Nien Nunb"]
+        self.assertEqual(actual, expected)
 
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -71,3 +78,58 @@ def test_import_all_into_mongo(self, mock_mongo_client):
 
     # Assertions
     mock_collection.insert_many.assert_called_once_with(sample_ships)
+
+
+import unittest
+from your_module import find_character_name, find_character_id, find_id_from_url
+
+# Replace with your actual character_dict and db configurations for testing
+character_dict = {
+    "url1": "Character 1",
+    "url2": "Character 2",
+}
+
+
+#### from Ruths
+class TestCharacterFunctions(unittest.TestCase):
+
+    def test_find_character_name(self):
+        character_url = "url1"
+        expected_name = "Character 1"
+
+        character_name = find_character_name(character_url)
+
+        self.assertEqual(character_name, expected_name)
+
+    def test_find_character_id(self):
+        character_name = "Character 1"
+
+        mock_db = {
+            "characters": [
+                {"name": "Character 1", "_id": "mock_id_1"},
+                {"name": "Character 2", "_id": "mock_id_2"},
+            ]
+        }
+
+        # function name from our code
+        character_id = find_character_id(character_name)
+
+        self.assertEqual(character_id, {"_id": "mock_id_1"})
+
+    def test_find_id_from_url(self):
+        character_url = "url1"
+
+        mock_db = {
+            "characters": [
+                {"name": "Character 1", "_id": "mock_id_1"},
+                {"name": "Character 2", "_id": "mock_id_2"},
+            ]
+        }
+        expected_id = {"_id": "mock_id_1"}
+        character_id = find_id_from_url(character_url)  # what the function is called in pur cpde
+
+        self.assertEqual(character_id, expected_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
