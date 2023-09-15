29c30befcdeda9864d7ea1e750143f547be3f5bb
Karis Reimers <karisjreimers@outlook.com>
2023-09-15 14:39:43 +0100

    Updated comments in main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ data = fetch_starships()
 print(data)
 write_into_json_file("starships.json", data)
 
-# Downloads all starships, then for each pilot URL downloads the pilot data
-# Groups pilots by starships
+# Downloads all starships, then returns keys: URL and values: pilot name
 pilots = fetch_names_for_pilots(pilots_for_starships(data))
 print(pilots)
 write_into_json_file("pilots.json", pilots)
