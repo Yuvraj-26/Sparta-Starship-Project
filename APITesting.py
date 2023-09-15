4f219fb0a73c70beb377e8556b919a6aa436ecb3
Andrew Carver <andyc2901@gmail.com>
2023-09-15 10:28:46 +0100

    cleaned the file and refined tests for pilot functions

--- APITesting.py
+++ APITesting.py
@@ -1,6 +1,7 @@
 import unittest
 import pymongo
 import json
+from your_module import find_character_name, find_character_id, find_id_from_url
 
 
 class Tests(unittest.TestCase):
@@ -29,6 +30,7 @@ class Tests(unittest.TestCase):
     def test_pilot_api_request(self):
         """
         Takes the "function" function and checks the URLs have been replaces by character names
+        TESTS DIRECT REPLACEMENT IN JSON
         """
         actual = function()['Millennium Falcon']
         expected = ["Chewbacca",
@@ -37,57 +39,14 @@ class Tests(unittest.TestCase):
                     "Nien Nunb"]
         self.assertEqual(actual, expected)
 
-
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-characters_collection = db["characters"]
-
-
-def test_match_pilot_with_character():
-    # Create a mock character in the characters collection
-    characters_collection.insert_one({"name": "Luke Skywalker", "_id": "mock_id"})
-
-    # Test matching a pilot's name with a character in the collection
-    pilot_name = "Luke Skywalker"
-    character = match_pilot_with_character(pilot_name, characters_collection)
-    assert character is not None
-    assert character["_id"] == "mock_id"
-
-
-def test_change_pilot_url_to_object_id():
-    # Create a mock character in the characters collection
-    characters_collection.insert_one({"name": "Pilot 1", "_id": "mock_id"})
-
-    # Test changing a pilot's URL to an Object ID
-    pilot_url = "https://example.com/pilots/1"
-    object_id = change_pilot_url_to_object_id(pilot_url, characters_collection)
-    assert object_id is not None
-    assert object_id == "mock_id"
-
-
-def test_import_all_into_mongo(self, mock_mongo_client):
-    # Mock MongoDB client and collection
-    mock_db = mock_mongo_client.return_value
-    mock_collection = mock_db['test_collection']
-
-    # Create sample ships
-    sample_ships = [{"name": "Ship 1"}, {"name": "Ship 2"}]
-
-    # Call the function to import all ships into MongoDB
-    import_all_into_mongo(mock_collection, sample_ships)
-
-    # Assertions
-    mock_collection.insert_many.assert_called_once_with(sample_ships)
-
-
-import unittest
-from your_module import find_character_name, find_character_id, find_id_from_url
-
-# Replace with your actual character_dict and db configurations for testing
-character_dict = {
-    "url1": "Character 1",
-    "url2": "Character 2",
-}
+    def test_URL_pilot_dictionary(self):
+        """
+        Takes the "function" function and checks the URL corresponds to the right name in the dictionary
+        TESTS DICTIONARY
+        """
+        actual = function()["https://swapi.dev/api/people/13/"]
+        expected = "Chewbacca"
+        self.assertEqual(actual, expected)
 
 
 #### from Ruths
@@ -126,7 +85,7 @@ class TestCharacterFunctions(unittest.TestCase):
             ]
         }
         expected_id = {"_id": "mock_id_1"}
-        character_id = find_id_from_url(character_url)  # what the function is called in pur cpde
+        character_id = find_id_from_url(character_url)  # what the function is called in our code
 
         self.assertEqual(character_id, expected_id)
 
